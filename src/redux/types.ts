eeb5855945bc639d0782a3857febc3e1421a6350
Cintia Arce <cintiaayelenarce@gmail.com>
2022-08-26 17:28:47 -0300

    add types all props

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -44,46 +44,125 @@ export interface Pokemon {
 
 export interface Move {
   accuracy: number;
+  contest_combos: {
+    normal: {
+      use_after: Array<{ name: string; url: string }>;
+      use_before: Array<{ name: string; url: string }>;
+    };
+    super: {
+      use_after: Array<{ name: string; url: string }>;
+      use_before: Array<{ name: string; url: string }>;
+    };
+  };
+  contest_effect: { url: string };
+  contest_type: { url: string };
+  damage_class: { name: string; url: string };
+  effect_chance: number;
+  effect_changes: Array<{
+    effect_entries: Array<{
+      effect: string;
+      short_effect?: string;
+      language: { name: "en" };
+    }>;
+    version_group: { name: string; url: string };
+  }>;
+  effect_entries: Array<{
+    effect: string;
+    short_effect?: string;
+    language: { name: "en" };
+  }>;
+  flavor_text_entries: Array<{
+    flavor_text: string;
+    version_group: { name: string; url: string };
+    language: { name: "en" };
+  }>;
+  generation: { name: string; url: string };
+  id: number;
+  learned_by_pokemon: Pokemon[];
+  machines: [];
+  meta: {
+    ailment: { name: string; url: string };
+    ailment_chance: number;
+    category: { name: string; url: string };
+    crit_rate: number;
+    drain: number;
+    flinch_chance: number;
+    healing: number;
+    stat_chance: number;
+  };
   name: string;
   power: number;
   pp: number;
   priority: number;
-  damageClass: string;
-  type: string;
+  target: { name: string; url: string };
+  type: { name: string; url: string };
 }
 
 export interface Species {
-  baseHappiness: number;
-  captureRate: number;
-  color: string;
+  base_happiness: number;
+  capture_rate: number;
+  color: { name: string; url: string };
+  egg_groups: Array<{ name: string; url: string }>;
+  evolution_chain: { url: string };
+  flavor_text_entries: Array<{}>;
+  forms_switchable: boolean;
+  gender_rate: number;
+  generation: { name: string; url: string };
+  growth_rate: { name: string; url: string };
+  habitat: { name: string; url: string };
+  hatch_counter: number;
+  id: number;
+  is_baby: boolean;
+  is_legendary: boolean;
+  is_mythical: boolean;
   name: string;
-  isBaby: boolean;
-  isLegendary: boolean;
-  isMythical: boolean;
-  formsSwitchable: boolean;
-  growthRate: string;
-  habitat: string;
-  shape: string;
+  order: number;
+  pal_park_encounter: Array<{
+    area: { name: string; url: string };
+    base_score: number;
+    rate: number;
+  }>;
+  pokedex_numbers: Array<{
+    entry_number: number;
+    pokedex: { name: string; url: string };
+  }>;
+  shape: { name: string; url: string };
+  varieties: Array<{ pokemon: Pokemon }>;
 }
 
 export interface Type {
   damageRelations: {
-    doubleDamageFrom: Array<string> | null;
-    doubleDamageTo: Array<string> | null;
-    halfDamageFrom: Array<string> | null;
-    halfDamageTo: Array<string> | null;
-    noDamageFrom: Array<string> | null;
-    noDamageTo: Array<string> | null;
+    doubleDamageFrom: Array<{ name: string; url: string }>;
+    doubleDamageTo: Array<{ name: string; url: string }>;
+    halfDamageFrom: Array<{ name: string; url: string }>;
+    halfDamageTo: Array<{ name: string; url: string }>;
+    noDamageFrom: Array<{ name: string; url: string }>;
+    noDamageTo: Array<{ name: string; url: string }>;
   };
-  moveDamageClass: string;
+  game_indices: Array<{
+    game_index: number;
+    generation: { name: string; url: string };
+  }>;
+  generation: { name: string; url: string };
+  move_damage_class: { name: string; url: string };
+  moves: Move[];
   name: string;
+  pokemon: Pokemon[];
 }
 
 export interface Item {
-  name: string;
+  attributes: Array<{ name: string; url: string }>;
+  category: { name: string; url: string };
   cost: number;
-  attributes: Array<string>;
-  sprites: string;
+  effect_entries: Array<{ name: string; url: string }>;
+  flavor_text_entries: Array<{ name: string; url: string }>;
+  fling_effect: number;
+  fling_power: number;
+  game_indices: Array<{ name: string; url: string }>;
+  held_by_pokemon: Pokemon[];
+  id: number;
+  name: string;
+  sprites: { default: string };
 }
 
 export interface PokemonState {
